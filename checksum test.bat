9089bf0d29ca2fecd82745dfe1a7a0a2fefe8884
ninjaman92 <58124810+ninjaman92@users.noreply.github.com>
2026-01-09 16:08:20 -0500

    Update checksum test.bat
    added comments to show what code does what and why

--- checksum test.bat
+++ checksum test.bat
@@ -1,16 +1,21 @@
 @echo off
 set /p input=Enter expected SHA256 checksum: 
 set /p dir=Enter full path to the file: 
+REM above sets the variables for input and dir, respectively 
 
 for /f "tokens=1" %%a in ('certutil -hashfile "%dir%" SHA256 ^| find /v "certutil" ^| find /v "hash"') do (
     set "output=%%a"
 )
+REM above creates a variable based on the output of the command, while also removing the words certutil and hash
+
 if %input% == %output% (
 echo is similar;  %output%
 ) else (
-echo faild to verify, its actually
+echo failed to verify, it's actually
 echo %output%
 echo vs expected checksum of
 echo %input%
 )
+REM above allows you to compare input and output using if else
+
 pause
